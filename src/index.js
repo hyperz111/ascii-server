ae8cc4a79aa62798f6c0fe2d3996952b626c02f5
hyperz111 <firzeroun11@gmail.com>
2026-05-01 05:44:55 +0700

    feat: add error handler

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const PORT = Number(process.env.PORT) || 8080;
 const INTERVAL = Number(process.env.INTERVAL) || 100;
 
 const server = new Server();
+const listeningListener = () => {
+  print(`Listen to port ${PORT}`);
+};
 
 server.on("request", (request, response) => {
   handler(request, response, INTERVAL);
@@ -13,6 +16,19 @@ server.on("request", (request, response) => {
   print(`${request.method} ${response.statusCode} ${request.url}`);
 });
 
-server.listen(PORT, () => {
-  print(`Listen to port ${PORT}`);
+let restartTimeout;
+
+server.on("error", (error) => {
+  if (error.code === "EADDRINUSE") {
+    print("Address in use, retrying...", "err");
+
+    restartTimeout = setTimeout(() => {
+      clearInterval(restartTimeout);
+      server.listen(PORT, listeningListener);
+    }, 5000);
+  }
+
+  throw error;
 });
+
+server.listen(PORT, listeningListener);
