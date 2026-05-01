ccbdc58f0c96d1f3b9e170b05e05d5674f12834d
hyperz111 <firzeroun11@gmail.com>
2026-05-01 17:40:51 +0700

    refactor: add more code comments for explanation

--- src/handler.js
+++ src/handler.js
@@ -8,7 +8,7 @@ import frames from "./frames/$index.js";
 /** @import { ServerResponse, IncomingMessage } from "node;http" */
 
 /**
- * Helper for JSON response.
+ * Helper for send JSON response.
  * @param {ServerResponse} response
  * @param {number} status
  * @param {unknown} jsonish
@@ -27,23 +27,36 @@ const sendJson = (response, status, jsonish) =>
  * @returns {void}
  */
 const handler = (request, response, interval) => {
+  // Check if the method is GET and request URL is available
   if (request.method === "GET" && typeof request.url === "string") {
+    // Parse
     const parsed = parse(request.url);
 
+    /**
+     * Possible paths:
+     * - /
+     * - /parrot
+     * - /parrot?what
+     */
     if (parsed.root === parsed.dir) {
+      // Get the frames data
       const frame = frames[parsed.base];
 
+      // If available
       if (Array.isArray(frame)) {
+        // If client use `curl` command
         if (request.headers["user-agent"]?.startsWith?.("curl/")) {
           let i = 0;
 
           response.writeHead(200, { "Transfer-Encoding": "chunked" });
 
+          // Start animation
           const ref = setInterval(() => {
             response.write(frame[i]);
             i = (i + 1) % frame.length;
           }, interval);
 
+          // Stop the animation on client disconnect
           response.on("close", () => {
             print("Client disconnected");
             clearInterval(ref);
@@ -53,12 +66,14 @@ const handler = (request, response, interval) => {
           return;
         }
 
+        // For non-curled client
         sendJson(response, 417, { error: `Almost done, curl it!` });
         return;
       }
     }
   }
 
+  // Not found
   sendJson(response, 404, { error: `Frame not found` });
   return;
 };
