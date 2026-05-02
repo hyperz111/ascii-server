ab973086f8d78d6b0b8c6b8fa4ee0da7ffacdce1
hyperz111 <firzeroun11@gmail.com>
2026-05-02 17:13:37 +0700

    chore: use WHATWG URL API

--- src/handler.js
+++ src/handler.js
@@ -2,7 +2,6 @@
  * @description Request handler module
  */
 
-import { parse } from "node:path/posix";
 import { print } from "./utils.js";
 import frames from "./frames/$index.js";
 /** @import { ServerResponse, IncomingMessage } from "node;http" */
@@ -30,46 +29,40 @@ const handler = (request, response, interval) => {
   // Check if the method is GET and request URL is available
   if (request.method === "GET" && typeof request.url === "string") {
     // Parse
-    const parsed = parse(request.url);
+    const parsed = new URL(`http://localhost${request.url}`);
+    // Get the frames data
+    const frame =
+      parsed.hash === "" &&
+      parsed.search === "" &&
+      frames[parsed.pathname.slice(1)];
 
-    /**
-     * Possible paths:
-     * - /
-     * - /parrot
-     * - /parrot?what
-     */
-    if (parsed.root === parsed.dir) {
-      // Get the frames data
-      const frame = frames[parsed.base];
+    // If available
+    if (Array.isArray(frame)) {
+      // If client use `curl` command
+      if (request.headers["user-agent"]?.startsWith?.("curl/")) {
+        let i = 0;
 
-      // If available
-      if (Array.isArray(frame)) {
-        // If client use `curl` command
-        if (request.headers["user-agent"]?.startsWith?.("curl/")) {
-          let i = 0;
+        response.writeHead(200, { "Transfer-Encoding": "chunked" });
 
-          response.writeHead(200, { "Transfer-Encoding": "chunked" });
+        // Start animation
+        const ref = setInterval(() => {
+          response.write(frame[i]);
+          i = (i + 1) % frame.length;
+        }, interval);
 
-          // Start animation
-          const ref = setInterval(() => {
-            response.write(frame[i]);
-            i = (i + 1) % frame.length;
-          }, interval);
+        // Stop the animation on client disconnect
+        response.on("close", () => {
+          print("Client disconnected");
+          clearInterval(ref);
+          response.end();
+        });
 
-          // Stop the animation on client disconnect
-          response.on("close", () => {
-            print("Client disconnected");
-            clearInterval(ref);
-            response.end();
-          });
-
-          return;
-        }
-
-        // For non-curled client
-        sendJson(response, 417, { error: `Almost done, curl it!` });
         return;
       }
+
+      // For non-curled client
+      sendJson(response, 417, { error: `Almost done, curl it!` });
+      return;
     }
   }
 
